a4ffcb4a69d23b4ed7b19eeb188fcb13fd680ac1
jijinbei <hikoharu14142@outlook.jp>
2023-07-15 14:48:00 +0900

    catcodeでwebview勉強

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,51 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+
+const cats = {
+  'codingcat': 'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
+  'compilingcat': 'https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif'
+};
+
+
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand('typing_game.start', () => {
+      // Create and show a new webview
+      const panel = vscode.window.createWebviewPanel(
+        'typing', // Identifies the type of the webview. Used internally
+        'タイピングゲーム', // Title of the panel displayed to the user
+        vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
+        {} // Webview options. More on these later.
+      );
+      
+      let iteration = 0;
+      const updateWebview = () => {
+        const cat = iteration++ % 2 ? 'codingcat' : 'compilingcat';
+        panel.title = cat;
+        panel.webview.html = getWebviewContent(cat);
+      };
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "typing-game" is now active!');
+      // Set initial content
+      updateWebview();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('typing-game.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from typing-game!');
-	});
-
-	context.subscriptions.push(disposable);
+      // And schedule updates to the content every second
+      setInterval(updateWebview, 1000);
+    })
+  );
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
+
+function getWebviewContent(cat: keyof typeof cats) {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="${cats[cat]}" width="300" />
+</body>
+</html>`;
+}
