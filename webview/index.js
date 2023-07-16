97947b598167ac7598043d9a923f187fac03fdf5
swawa-yu <swawa.yu@gmail.com>
2023-07-17 01:11:00 +0900

    コードをきれいに

--- webview/index.js
+++ webview/index.js
@@ -38,9 +38,6 @@ let fires = [];
 
 // クリボーとファイアボールが衝突しているかどうかを判定
 function isCollision(goomba, fire) {
-    if (goomba === null || fire === null) {
-        return false;
-    }
     if (goomba.x <= fire.x) {
         return true;
     } else {
@@ -60,18 +57,15 @@ function pixiGame() {
 
     // create a bounding box for the little dudes
     const dudeBoundsPadding = 100;
-    const dudeBounds = new PIXI.Rectangle(-dudeBoundsPadding,
-        -dudeBoundsPadding,
-        app.screen.width + dudeBoundsPadding * 2,
-        app.screen.height + dudeBoundsPadding * 2);
+    const dudeBounds = new PIXI.Rectangle(
+        -dudeBoundsPadding, -dudeBoundsPadding,
+        app.screen.width + dudeBoundsPadding * 2, app.screen.height + dudeBoundsPadding * 2);
 
     // クリボーはファイアボールか左端の壁に当たったときに消える
     // ファイアボールはクリボーか壁に当たったときに消える
     app.ticker.add(() => {
         // update goombas
-        console.log("hoge", goombas, fires)
         goombas.forEach(goomba => {
-            console.log(goomba)
             goomba.x += Math.sin(goomba.direction) * goomba.speed;
             goomba.y += Math.cos(goomba.direction) * goomba.speed;
         });
@@ -80,9 +74,9 @@ function pixiGame() {
         fires.forEach(fire => {
             fire.x += Math.sin(fire.direction) * fire.speed;
             fire.y += Math.cos(fire.direction) * fire.speed;
-            // if (fire.x > dudeBounds.x + dudeBounds.width) {
-            //     fire.destroy();
-            // }
+            if (fire.x > dudeBounds.x + dudeBounds.width) {
+                fire.destroy();
+            }
         });
 
         // // 衝突判定と削除の処理
@@ -105,10 +99,8 @@ function pixiGame() {
 }
 
 
-// let textChangedCount = 0;
 window.addEventListener('message', event => {
     const message = event.data;
-    console.log(event);
     if (message.type === 'init') {
         const { goombaUri, fireUri } = message.value;
         goombauri = goombaUri;
